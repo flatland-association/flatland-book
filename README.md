99ede58164cfbce4c14d6f0c12a00c1668e5b625
chenkins <chenkins44@gmail.com>
2025-03-19 10:48:51 +0100

    Execute notebooks, use build.sh in ci.

--- README.md
+++ README.md
@@ -9,20 +9,10 @@ the [Flatland-RL](https://github.com/flatland-association/flatland-rl/) project.
 ```shell
 git clone https://github.com/flatland-association/flatland-book.git 
 
+cd flatland-book
 pip install -U -r requirements.txt
 
-FLATLAND_MODULE_PATH=$(python -c 'import os; import importlib; print(os.path.dirname(importlib.import_module("flatland").__file__))')
-FLATLAND_MODULE_VERSION=$(python -c "import flatland; print(flatland.__version__)")
-sphinx-apidoc --force -a -e -o apidocs ${FLATLAND_MODULE_PATH}  -H "Flatland ${FLATLAND_MODULE_VERSION} API Reference" --tocfile 'index'
-find . -name "*.md" -print0 | xargs -0  sed -i 's/```mermaid/```{mermaid}/g'
-# on macOS: use find . -name "*.md" -print0 | xargs -0  sed -i '' 's/```mermaid/```{mermaid}/g'
-jupyter-book build .
-```
-
-Changes to the ToC will require full re-build by running
-
-```shell
-jupyter-book clean .
+bash build.sh
 ```
 
 # Author(s)
--- build.sh
+++ build.sh
@@ -1,1 +1,19 @@
-jupyter-book clean .; jupyter book build .
+set -e
+set -x
+# avoid executing all notebooks
+# TODO https://github.com/flatland-association/flatland-rl/issues/132 make all notebooks executable unattended
+# TODO use notebooks from flatland-rl directly instead of duplicating in flatland-notebook repo?
+find venv -name "*.ipynb" -print0 | xargs -0 rm
+
+FLATLAND_MODULE_PATH=$(python -c 'import os; import importlib; print(os.path.dirname(importlib.import_module("flatland").__file__))')
+FLATLAND_MODULE_VERSION=$(python -c "import flatland; print(flatland.__version__)")
+sphinx-apidoc --force -a -e -o apidocs ${FLATLAND_MODULE_PATH}  -H "Flatland ${FLATLAND_MODULE_VERSION} API Reference" --tocfile 'index'
+
+# tweak mermaid directives for sphinx, see https://sphinxcontrib-mermaid-demo.readthedocs.io/en/latest/index.html
+# on macOS: use find . -name "*.md" -print0 | xargs -0  sed -i '' 's/```mermaid/```{mermaid}/g'
+find . -name "*.md" -print0 | xargs -0  sed -i 's/```mermaid/```{mermaid}/g'
+jupyter-book clean .
+jupyter book build .
+find _build -name "*.ipynb" -print0 | xargs -0 fgrep ImportError
+NUM=$(find _build -name "*.ipynb" -print0 | xargs -0 fgrep ImportError | wc -l)
+exit ${NUM}
