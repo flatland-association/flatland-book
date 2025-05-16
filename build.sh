bce3c4e34e0ccd93d0d18c3b7b945150a1df1ee1
Christian Eichenberger <chenkins44@gmail.com>
2025-05-16 17:51:58 +0200

    Run Agent-Close-Following.ipynb in gh. (#18)

--- build.sh
+++ build.sh
@@ -1,19 +1,12 @@
 set -e
 set -x
-# avoid executing all notebooks
-# TODO https://github.com/flatland-association/flatland-rl/issues/132 make all notebooks executable unattended
-# TODO use notebooks from flatland-rl directly instead of duplicating in flatland-notebook repo?
 
 FLATLAND_MODULE_PATH=$(python -c 'import os; import importlib; print(os.path.dirname(importlib.import_module("flatland").__file__))')
 FLATLAND_MODULE_VERSION=$(python -c "import flatland; print(flatland.__version__)")
 
-# TODO copy from flatland-rl - which ones to run and which ones to cache?
-cp assets/images/graph_to_digraph.drawio.png environment/environment/images/graph_to_digraph.drawio.png
 cp ${FLATLAND_MODULE_PATH}/../notebooks/graph_demo.ipynb environment/environment
-
-#cp ${FLATLAND_MODULE_PATH}/../notebooks/Agent-Close-Following.ipynb environment/environment
-# TODO waiting for 4.0.7 pr and copy from there
-#cp -R ${FLATLAND_MODULE_PATH}/../notebooks/images/ environment/environment/images/
+cp ${FLATLAND_MODULE_PATH}/../notebooks/Agent-Close-Following.ipynb environment/environment
+cp -R ${FLATLAND_MODULE_PATH}/../notebooks/images/ environment/environment/images/
 
 sphinx-apidoc --force -a -e -o apidocs ${FLATLAND_MODULE_PATH}  -H "Flatland ${FLATLAND_MODULE_VERSION} API Reference" --tocfile 'index'
 
